5abb1f1514fd68e890fd4e78445dc8e34099d841
fedelopre <lpfederico40@gmail.com>
2025-08-25 15:44:14 +0000

    Now vel_mod build

--- src/vel_modifier/src/velocity_modifier.cpp
+++ src/vel_modifier/src/velocity_modifier.cpp
@@ -1,21 +1,22 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 class VelSubscriber : public rclcpp::Node
 {
     int direction; // quanto è lontano l'oggetto che sto raggiungendo in quella direzione
+    rclcpp::TimerBase::SharedPtr timer_;
 public:
     VelSubscriber() : Node("velocity_modifier")
     {
-        subscriber_vel = this->create_subscription<std_msgs::msg::String>(
-            "vel", 10,
-            std::bind(&VelSubscriber::callback, this, std::placeholders::_1));
-        
-        scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-            "/scan", 10, std::bind(&VelSubscriber::callback_scan, this, _1));
+        timer_ = this->create_wall_timer(
+            std::chrono::milliseconds(100), std::bind(&VelSubscriber::Velocity, this));
 
-        publisher_ = this->create_publisher<std_msgs::msg::String>("vel_modificata", 10);
+        scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+            "/scan", 10, std::bind(&VelSubscriber::callback_scan, this, std::placeholders::_1));
+
+        publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/nav_vel", 10);
         direction = 0.1;
     }
 
@@ -26,34 +27,41 @@ private:
     /* 
         Serve per la navigazione normale e prende la velocità e la scala per la direction(distanza)
     */
-    void callback(const std_msgs::msg::String::SharedPtr msg)
+    void Velocity()
     {    
         /* decidi se spostarlo in un timed Loop oppure tenere la call back della velocità */
         //qua modifica la velocità
-        std_msgs::msg::String nuovo_msg;
-        std::lock_guard<std::mutex> lock(m);
-        nuovo_msg.data = 0.3*direction;
+        auto cmd = geometry_msgs::msg::Twist();
 
-        RCLCPP_INFO(this->get_logger(), "Pubblico Velocità modificata: '%s'", nuovo_msg.data.c_str());
-        publisher_->publish(nuovo_msg);
+        std::unique_lock<std::mutex> lock(m);
+        cmd.linear.x = 0.3*direction;
+
+        publisher_->publish(cmd);
     }
 
     /* Prende la distanza forntale e la mappa in un valore tra 0 e 1*/
-    void callback_scan(const std_msgs::msg::String::SharedPtr msg)
+    void callback_scan(const sensor_msgs::msg::LaserScan::SharedPtr msg)
     {
         size_t mid_index = msg->ranges.size() / 2; //davanti
         float front = msg->ranges[mid_index]; 
-        std::lock_guard<std::mutex> lock(m);
         // mappare da msg->range_max a range_min  a 1 a 0
-        if (float('inf') == front){
+        if (std::isinf(front)){
             front = 1;
         } else{ 
             front = (front - msg->range_min) / (msg->range_max - msg->range_min);
         }
-        std::lock_guard<std::mutex> lock(m);
+        std::unique_lock<std::mutex> lock(m);
         direction = front;
     }
 
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_vel;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scan_sub_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<VelSubscriber>());
+    rclcpp::shutdown();
+    return 0;
+}
